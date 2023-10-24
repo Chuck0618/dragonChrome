b3b623769ba666ea1cec49918bcb7df6f242090b
hcq_home <7fx0618@163.com>
2023-10-25 00:15:23 +0800

    some example

--- src/Demo.bs.js
+++ src/Demo.bs.js
@@ -6,9 +6,40 @@ console.log("Hello, World!");
 
 console.log("hello Rescript");
 
-var d = 4.2 + 1.2;
+var d = "newd";
 
-console.log(d);
+console.log("b: int");
+
+console.log("number");
+
+var cond = {
+  contents: true
+};
+
+console.log(cond);
+
+console.log(typeof d);
+
+cond.contents = false;
+
+cond.contents = true;
+
+function add(a, b) {
+  return a + b | 0;
+}
+
+function minus(a, b) {
+  return a - b | 0;
+}
+
+var Int = {
+  add: add,
+  minus: minus
+};
+
+console.log(3);
+
+console.log(undefined);
 
 var a = "hello";
 
@@ -16,8 +47,13 @@ var b = 4234;
 
 var c = 4.2;
 
+var cc;
+
 exports.a = a;
 exports.b = b;
 exports.c = c;
 exports.d = d;
+exports.cond = cond;
+exports.Int = Int;
+exports.cc = cc;
 /*  Not a pure module */
--- src/Demo.res
+++ src/Demo.res
@@ -8,5 +8,21 @@ let b:int = 4234;
 let c:float = 4.2;
 
 let d= c +. 1.2
-Js.log(d)
+let d = "newd"
+Js.log("b: int")
+Js.log(Js.typeof(b))
+let cond = ref(true)
+Js.log(cond);
+Js.log(Js.typeof(d));
+cond.contents = false ;
 
+cond:= true;
+module Int = {
+   let add= (a,b)=>{a + b}
+   let minus = (a,b) =>{a -b }
+    // Module contents
+}
+
+let cc=Js.log(Int.add(1,2));
+// Js.log(Js.typeof(Int));
+Js.log(cc)
